fd9a0a0fcaf693e1d47151cc4ed12ace7d42c8e0
Neroimor <neroimor@gmail.com>
2024-11-22 16:36:17 +0300

    rebuilt token system

--- src/main/java/com/neroimor/ImageLibrary/Components/JWTComponent/JwtAuthenticationFilter.java
+++ src/main/java/com/neroimor/ImageLibrary/Components/JWTComponent/JwtAuthenticationFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -30,12 +32,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         String token = extractTokenFromHeader(request);
 
         if (token != null && jwtTokenProvider.validateToken(token)) {
             String email = jwtTokenProvider.getEmailFromToken(token);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, new ArrayList<>());
+            String role = jwtTokenProvider.getRoleFromToken(token);  // Получаем роль из токена
+            // Создаем роль для пользователя
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role); // Добавляем префикс 'ROLE_' если нужно
+            // Создаем объект Authentication с ролью
+            Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, List.of(authority));
+            // Устанавливаем аутентификацию в SecurityContext
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
